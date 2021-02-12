de88f4f0f00e4e2c3f73ba796d7c0d65248e6296
Zachary Bray <zachbray9@knights.ucf.edu>
2021-02-11 21:44:18 -0500

    Working first person player controller

---
+++ Hallway With Guard/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    private Vector3 movement;
+    private Vector3 velocity;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+    private float horizontal;
+    private float vertical;
+    private bool isGrounded;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        horizontal = Input.GetAxis("Horizontal"); 
+        vertical = Input.GetAxis("Vertical");  
+
+        movement = transform.right * horizontal + transform.forward * vertical;
+
+        controller.Move(movement * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
