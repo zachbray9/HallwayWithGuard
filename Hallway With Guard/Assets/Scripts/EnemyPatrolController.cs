720f61b108f43b48e3caf51d0dd6a12d811a0013
Zachary Bray <zachbray9@knights.ucf.edu>
2021-02-18 19:16:06 -0500

    NavMesh works :)))

--- Hallway With Guard/Assets/Scripts/EnemyPatrolController.cs
+++ Hallway With Guard/Assets/Scripts/EnemyPatrolController.cs
@@ -5,5 +5,34 @@ using UnityEngine.AI;
 
 public class EnemyPatrolController : MonoBehaviour
 {
-    
+    public NavMeshAgent agent;
+
+    public Transform[] patrolLocations;
+
+    private int destPoint = 0; 
+
+    void Start()
+    {
+        moveToNextLocation();
+    }
+
+    void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            moveToNextLocation();
+        }
+    }
+
+    void moveToNextLocation()
+    {
+        if(patrolLocations.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = patrolLocations[destPoint].position;
+
+        destPoint = (destPoint + 1) % patrolLocations.Length;
+    }
 }
