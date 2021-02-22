9b149c4ff80e77cd7e1b314b917dc5ffbf329052
Zachary Bray <zachbray9@knights.ucf.edu>
2021-02-22 17:38:54 -0500

    Working on Main Menu

---
+++ Hallway With Guard/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        //SceneManager.LoadScene();                                     //Add the correct scene to the function
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
