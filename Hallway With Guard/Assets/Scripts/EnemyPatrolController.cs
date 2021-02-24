0cf3e7719d994bdcbd3c01e22049026bb6c7fc5e
Zachary Bray <zachbray9@knights.ucf.edu>
2021-02-24 18:40:25 -0500

    Models are now controlled and chicken AI is terrifying

--- Hallway With Guard/Assets/Scripts/EnemyPatrolController.cs
+++ Hallway With Guard/Assets/Scripts/EnemyPatrolController.cs
@@ -6,26 +6,51 @@ using UnityEngine.AI;
 public class EnemyPatrolController : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public Transform player;
 
     public Transform[] patrolLocations;
 
+    public float maxRange;
     private int destPoint = 0; 
+
+
 
     void Start()
     {
         moveToNextLocation();
     }
 
+
+
     void Update()
     {
-        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        
+        float currentDistance = Vector3.Distance(transform.position, player.position);
+
+        if(checkIfPlayerInSight() && isInLineOfSight())
         {
-            moveToNextLocation();
+            if(currentDistance < maxRange)
+            {
+                //chicken chases player
+                agent.SetDestination(player.position);
+                transform.LookAt(player);
+            }
         }
+        else
+        {
+            if(!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                moveToNextLocation();
+            }
+        }
+        
     }
+
+
 
     void moveToNextLocation()
     {
+
         if(patrolLocations.Length == 0)
         {
             return;
@@ -34,5 +59,46 @@ public class EnemyPatrolController : MonoBehaviour
         agent.destination = patrolLocations[destPoint].position;
 
         destPoint = (destPoint + 1) % patrolLocations.Length;
+
     }
+
+
+
+    bool checkIfPlayerInSight()
+    {
+
+        Vector3 directionOfPlayer = transform.position - player.position;
+        float angle = Vector3.Angle(transform.forward, directionOfPlayer);
+
+        if(Mathf.Abs(angle) > 90 && Mathf.Abs(angle) < 270)
+        {
+            //Debug.DrawLine(transform.position, player.position, Color.red);
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+
+    bool isInLineOfSight()
+    {
+
+        RaycastHit hit;
+        Vector3 directionOfPlayer = player.position - transform.position;
+
+        if(Physics.Raycast(transform.position, directionOfPlayer, out hit, 50000f))
+        {
+            if(hit.transform.name == "First Person Player")
+            {
+                Debug.DrawLine(transform.position, player.position, Color.green);
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
 }
