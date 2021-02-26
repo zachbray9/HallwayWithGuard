7691f0e812646288c8e908377d2854a4575ff400
Zachary Bray <zachbray9@knights.ucf.edu>
2021-02-25 20:11:28 -0500

    Working on win and lose conditions

---
+++ Hallway With Guard/Assets/Scripts/WinManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinManager : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if(collider.gameObject.name == "First Person Player")
+        {
+            Debug.Log("You win!");
+            //SceneManager.LoadScene();
+        }
+    }
+}
