49438b2f428e5b572c8bdc8ca19b11d2e2168ed8
Zachary Bray <zachbray9@knights.ucf.edu>
2021-02-28 00:11:48 -0500

    Added pause menu

---
+++ Hallway With Guard/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        Resume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = false;
+    }
+
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        isPaused = true;
+    }
+
+
+    public void LoadMenu()
+    {
+        isPaused = false;
+        pauseMenuUI.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
